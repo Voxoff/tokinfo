a77da18ddea2f529b93196e811b4a1b0dd132c9f
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-13 18:30:22 +0000

    seeded this motherfucker

--- db/migrate/20171113144140_create_tokens.rb
+++ db/migrate/20171113144140_create_tokens.rb
@@ -2,7 +2,7 @@ class CreateTokens < ActiveRecord::Migration[5.1]
   def change
     create_table :tokens do |t|
       t.string :name
-      t.references :businesses, foreign_key: true
+      t.references :business, foreign_key: true
 
       t.timestamps
     end
