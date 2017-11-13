f639d077568571e8a3c349ea3073c9eddad54439
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-13 21:30:13 +0000

    blah

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @business = Business.where(user_id: @user.id)
-    @token = Token.where(user_id = @user.id)
+    @token = Token.where(user_id: @user.id)
   end
 
   def index
