7c7e9acbcac3087f1b66f254e06e8e15911a223c
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-13 20:56:30 +0000

    admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@ require 'open-uri'
 url = "https://api.coinmarketcap.com/v1/ticker/"
 data = JSON.parse(open(url).read)
 
+# admin account for easy access
+  admin = User.create!(first_name: "admin", email: "admin@admin.com", password: "123123")
+  admin_business = Business.create!(user_id: admin.id, name: "admin", address: "admin street", url: "admin.com", email: "admin@admin.com")
+  Token.create!(name: "admin", business_id: admin_business.id)
 
 print "Seeding..."
 data.each_with_index do |coin, i|
