f7dcac5db70e7a1303e1d6986a630055942f7efd
leonard percival <leonardpercival@gmail.com>
2017-11-13 16:21:43 +0000

    added token methods

---
+++ app/controllers/tokens_controller.rb
@@ -0,0 +1,46 @@
+class TokensController < ApplicationController
+    before_action :set_token, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tokens = Token.all
+  end
+
+  def show
+  end
+
+  def new
+    @token = Token.new
+  end
+
+  def create
+    @token = Token.new(token_params)
+    @token.save
+    redirect_to token_path(@token)
+  end
+
+  def edit
+  end
+
+  def update
+    @token.update(token_params)
+    redirect_to token_path(@token)
+  end
+
+  def destroy
+    @token.destroy
+    redirect_to tokens_path
+  end
+
+  private
+
+
+  def token_params
+    params.require(:token).permit(:name, :image)
+  end
+
+  def set_token
+    @token = Token.find(params[:id])
+  end
+
+
+end
