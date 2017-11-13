f96ee3b6ba96b7261e77f9805e62b5c132f3016b
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-13 21:20:54 +0000

    adde user id to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ data = JSON.parse(open(url).read)
 # admin account for easy access
   admin = User.create!(first_name: "admin", email: "admin@admin.com", password: "123123")
   admin_business = Business.create!(user_id: admin.id, name: "admin", address: "admin street", url: "admin.com", email: "admin@admin.com")
-  Token.create!(name: "admin", business_id: admin_business.id)
+  Token.create!(name: "admin", business_id: admin_business.id, user_id: admin.id)
 
 print "Seeding..."
 data.each_with_index do |coin, i|
@@ -15,7 +15,7 @@ data.each_with_index do |coin, i|
     # need a user and business for each token
     user = User.create!(first_name: Faker::Name.name, email: Faker::Internet.email, password: "123123")
     business = Business.create!(user_id: user.id, name: Faker::Company.name, address: Faker::Address.city, url: Faker::Internet.url, email: Faker::Internet.email)
-    Token.create!(name: coin["name"], business_id: business.id)
+    Token.create!(name: coin["name"], business_id: business.id, user_id: user.id)
     print "."
   end
 end
