49aa02dff6f38935403f1bcf6b9072b48f90ceb2
leonard percival <leonardpercival@gmail.com>
2017-11-14 13:39:59 +0000

    added user view and

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
   def show
     @user = User.find(params[:id])
     @business = Business.where(user_id: @user.id)
-    @token = Token.where(user_id: @user.id)
+    @tokens = Token.where(user_id: @user.id)
   end
 
   def index
