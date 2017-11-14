12cadc2e7a02b07688582041bdd1b4c3bfa20a8d
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-14 12:31:05 +0000

    images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113182155) do
+ActiveRecord::Schema.define(version: 20171114122904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171113182155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "photo"
     t.index ["business_id"], name: "index_tokens_on_business_id"
     t.index ["user_id"], name: "index_tokens_on_user_id"
   end
