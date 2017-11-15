95ab9dd53f993e6ab17a235c8ad83ab27b1719aa
leonard percival <leonardpercival@gmail.com>
2017-11-15 15:33:22 +0000

    fixed no go problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:show, :index]
-<<<<<<< HEAD
-  resources :businesses do
-    resources :tokens, only: [:new, :create]
-  end
-  resources :tokens, only: [:index, :show, :destroy, :edit, :update]
-  # For details on the DSL available within this falseile, see http://guides.rubyonrails.org/routing.html
-=======
   resources :businesses
   resources :tokens
   resources :followings, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> a9755dbf3b085b3660af67893a4bb6a13c8258e4
 end
