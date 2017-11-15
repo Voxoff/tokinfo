6d10845b73c75b37d19bc18dfb1a6eb466461e9d
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-15 14:13:01 +0000

    can see other user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
   class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    authorize(@user)
     @business = Business.where(user_id: @user.id)
     @tokens = Token.where(user_id: @user.id)
   end
