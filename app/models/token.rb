0930776facdf847366235ecc3f4a88b83f1ef5c2
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-15 14:50:40 +0000

    schema second draft

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,5 +1,6 @@
 class Token < ApplicationRecord
   belongs_to :business
   has_many :followings
+  has_many :users, through: :followings
   mount_uploader :photo, PhotoUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   resources :businesses
   resources :tokens
+  resources :followings, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
