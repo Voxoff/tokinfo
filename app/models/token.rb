698d5559d96a7e24a0b541123457028dd597579d
leonard percival <leonardpercival@gmail.com>
2017-11-16 12:06:42 +0000

    added search function

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,4 +1,6 @@
 class Token < ApplicationRecord
+  include PgSearch
+  multisearchable against: [:name, :photo, :description]
   belongs_to :business
   has_many :followings
   has_many :users, through: :followings
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include PgSearch
+  multisearchable against: [:first_name, :last_name, :email, :photo, :description]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :followings
