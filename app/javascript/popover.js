29b31c1b97203da3e0ad2e95f7b36d6f6698085f
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-17 12:48:12 +0000

    popover and dropdown fix

--- app/javascript/popover.js
+++ app/javascript/popover.js
@@ -1,11 +1,3 @@
-// $(function () {
-//   $('[data-toggle="follow-btn"]').popover({
-//     container: '.btn',
-//     trigger:  "hover"
-//   })
-// })
-
-// $document.ready(function(){
-//   $('[data-toggle="popover"]').popover({trigger: "hover"});
-// });
-// $("#popover").popover({ trigger: "hover" });
+$(function () {
+  $('[data-toggle="popover"]').popover({trigger: "hover"})
+})
